216ec69888370b335edfae736d61abdfe107d44f
30260318 <zulyolivarez2017@gmail.com>
2018-09-01 10:08:15 -0600

    Add files via upload

---
+++ examen/encuesta/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>encuesta</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<center><form method=" formulario php" method="POST">
+		nombre:<input type="text" name="nombre">
+		genero:<input type="text" name="genero">
+		<br><br><br></center>
+	
+	<form method="formpost.php" method="POST">
+		Que lo motivo hacer catedratico <input type="text" name="Que lo motivo hacer catedratico">
+		Cuantos tiempo le llevo ser catedratico <input type="text" name="Cuantos tiempo le llevo ser catedratico">
+		Que es lo mas dificil de ser catedratico <input type="text" name="Que es lo mas dificil de ser catedratico">
+		
+		<br><br><br>
+		<form method="formpost.php" method="POST">
+			Cuanto lleva de ser catedratico <input type="text" name="Cuanto lleva de ser catedratico ">
+			Que edad es la mas dificil de los alumnos  <input type="text" name="Que edad es la mas dificil de los alumnos">
+			Cual es la diferencia enfre profesor y catedratico <input type="text" name="Cual es la diferencia enfre profesor y catedratico">
+<br><br><br>
+		<form method="formpost.php" method="POST">
+			Que beneficios tiene un catedratico <input type="text" name="Que beneficios tiene un catedratico">
+			Cuantas clases imparte <input type="text" name="Cuantas clases imparte">
+			Aparte de catedratico trabaja en algo mas <input type="text" name="Aparte de catedratico trabaja en algo mas">
+<br><br><br>
+			<form method="formpost.php" method="POST">
+				 Cuales son las contra de ser catedratico <input type="text" name="Cuales son las contra de ser catedratico">
+				 Es mas dificil trabajr con basicos o diversificado <input type="text" name="Es mas dificil trabajr con basicos o diversificado">
+				 Que clasaes le gusta impartir mas <input type="text" name="que clasaes le gusta impartir mas">	 
+<br><br><br>
+			<form method="formpost.php" method="POST">
+				Le gusta trabajar mas en la semana o fin de semana <input type="text" name="Le gusta trabajar mas en la semana o fin de semana">
+				Que obstaculo le a traido ser catedratico <input type="text" name="Que obstaculo le a traido ser catedratico">
+				Le gustaria algun dia ser director <input type="text" name="le gustaria algun dia ser director">
+<br><br><br>
+		<form method="formpost.php" method="POST">
+			Tiene pensado sacar alguna maestria o doctorado <input type="text" name="Tiene pensado sacar alguna maestria o doctorado">
+			Tiene pensado seguir estidiando <input type="text" name="Tiene pensado seguir estidiando">
+			A que edad piensa jubilarse <input type="text" name="A que edad piensa jubilarse">
+<br><br><br>		<form method="formpost.php" method="POST">
+		En donde labora le pagan IGSS <input type="text" name="En donde labora le pagan IGSS">
+			En donde labora tiene todas las prestaciones <input type="text" name="En donde labora tiene todas las prestaciones">
+			A trabajado para el gobierno <input type="text" name="A trabajado para el gobierno">
+<br><br><br>
+		<form method="formpost.php" method="POST">
+			A trabajado con analfabetas <input type="text" name="A trabajado con analfabetas">
+			Tienes beneficiosal trabajar con el gobierno <input type="text" name="Tienes beneficiosal trabajar con el gobierno">
+			Tiene beneficios al trabajar con la entidad privada <input type="text" name="Tiene beneficios al trabajar con la entidad privada">
+<br><br><br>	
+		<center>
+			<form method="formpost.php" method="POST">
+			Le gustaria algun dia tener su propia Insstitucion <input type="text" name="Le gustaria algun dia tener su propia Insstitucion">
+		</center>
+	<?php
+	
+	$Que lo motivo hacer catedratico =s_POST("Que lo motivo hacer catedratico");
+	$Cuantos tiempo le llevo ser catedratico =s_POST("Cuantos tiempo le llevo ser catedratico");
+	$Que es lo mas dificil de ser catedratico=s_POST("Que es lo mas dificil de ser catedratico");
+	$Cuanto lleva de ser catedratico=s_POST("Cuanto lleva de ser catedratico");
+	$Que edad es la mas dificil de los_alumnos =s_POST("Que edad es la mas dificil de los_alumnos");
+	$Cual es la diferencia enfre profesor y catedratico=s_POST("Cual es la diferencia enfre profesor y catedratico");
+	$Que beneficios tiene un catedratico=s_POST("Que beneficios tiene un catedratico");
+	$Cuantas clases imparte =s_POST("Cuantas clases imparte");
+	$Aparte de catedratico trabaja en algo mas=s_POST("Aparte de catedratico trabaja en algo mas");
+	$Cuales son las contra de ser catedratico=s_POST("Cuales son las contra de ser catedratico");
+	$Es mas dificil trabajr con basicos o diversificado=s_POST("Es mas dificil trabajr con basicos o diversificado");
+	$Que clasaes le gusta impartir mas=s_POST("Que clasaes le gusta impartir mas");
+	$Le gusta trabajar mas en la semana o fin de semana=s_POST("Le gusta trabajar mas en la semana o fin de semana");
+	$Que obstaculo le a traido ser catedratico=s_POST("Que obstaculo le a traido ser catedratico"); 
+	$Le gustaria algun dia ser director=s_POST("Le gustaria algun dia ser director");
+	$Tiene pensado sacar alguna maestria o doctorado=s_POST("Tiene pensado sacar alguna maestria o doctorado");
+	$Tiene pensado seguir estidiando=s_POST("Tiene pensado seguir estidiando");
+	$A que edad piensa jubilarse=s_POST("A que edad piensa jubilarse");
+	$En donde labora le pagan IGSS=s_POST("En donde labora le pagan IGSS");
+	$En donde labora tiene todas las prestaciones=s_POST("En donde labora tiene todas las prestaciones");
+	$A trabajado para el gobierno=s_POST("A trabajado para el gobierno");
+	$A trabajado con analfabetas=s_POST("A trabajado con analfabetas");
+	$Tienes beneficiosal trabajar con el gobierno=s_POST("Tienes beneficiosal trabajar con el gobierno");
+	$Tiene beneficios al trabajar con la entidad privada=s_POST("Tiene beneficios al trabajar con la entidad privada");
+	$Le gustaria algun dia tener su propia Insstitucion=s_POST("Le gustaria algun dia tener su propia Insstitucion");
+	?>
+</head>
+<body>
+
+</body>
+</html>
